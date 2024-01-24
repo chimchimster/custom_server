681ab4db5b8206f5f5637866af90f4bd27084605
chimchimster <jorddyishere@gmail.com>
2024-01-24 16:32:14 +0600

    trying adapter

--- server/server.py
+++ server/server.py
@@ -1,9 +1,8 @@
 import logging
+import queue
 import socket
 import pathlib
-
 import threading
-import time
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -15,9 +14,8 @@ class SockServer:
 
     def __init__(self, addr: str, port: int):
         self.__addr = (addr, port)
-        self.__server_socket = self.__create_socket()
         self.__logger = self.__set_logger()
-        self.__observer = FSObserver(self, self.__logger)
+        self.__server_socket = self.__create_socket()
 
     @property
     def log(self):
@@ -44,38 +42,25 @@ class SockServer:
         server_socket.listen()
         return server_socket
 
-    def reload_server(self):
-
-        self.log.info('Closing server socket... Reloading...')
-        self.__server_socket.close()
-
-        try:
-            self.__server_socket = self.__create_socket()
-        except OSError as e:
-            self.log.error(f"Failed to bind socket: {e}")
-            time.sleep(1)
-
-        self.start_serving()
-
     def start_serving(self):
+        with self.__server_socket as soc:
+            self.log.info('Server started on http://%s:%s' % self.__addr)
 
-        self.log.info('Server started on http://%s:%s' % self.__addr)
-
-        self.__observer.start_observer()
-
-        threads = []
-        try:
-            while True:
-                con, addr = self.__server_socket.accept()
-                addr: tuple
-                self.log.info('Received connection from %s:%s address.' % addr)
-                if con is not None:
-                    thr = threading.Thread(target=self.__handle_client, args=(con,))
-                    threads.append(thr)
-                    thr.start()
-        finally:
-            [thr.join() for thr in threads]
-            self.__observer.stop_observer()
+            threads = []
+            try:
+                while True:
+                    con, addr = soc.accept()
+                    addr: tuple
+                    if con is not None:
+                        self.log.info('Received connection from %s:%s address.' % addr)
+                        thr = threading.Thread(target=self.__handle_client, args=(con,))
+                        threads.append(thr)
+                        thr.start()
+            except KeyboardInterrupt:
+                self.log.info('Server interrupted by user.')
+            finally:
+                for thr in threads:
+                    thr.join()
 
     @staticmethod
     def __handle_client(connection):
@@ -90,52 +75,66 @@ class SockServer:
                 v = pars.parse_http_request()
                 print(v)
 
+        connection.close()
+
 
 class FSObserver:
 
     BASE_DIR = pathlib.Path().resolve()
+    OBSERVER = None
 
-    def __init__(self, serv, log):
-        self.__observer = None
-        self.__server_instance = serv
-        self.__logger_instance = log
+    def __init__(self, reload_queue: queue.Queue):
+        self.__reload_queue = reload_queue
 
     def start_observer(self):
-        self.__logger_instance.info('Hello')
-        if self.__observer is None or not self.__observer.is_alive():
-            self.__observer = Observer()
-            self.__observer.schedule(
-                ServerFileHandler(self.__server_instance, self.__logger_instance),
+        if self.OBSERVER is None or not self.OBSERVER.is_alive():
+            self.OBSERVER = Observer()
+            self.OBSERVER.schedule(
+                ServerFileHandler(self.__reload_queue),
                 path=str(self.BASE_DIR),
                 recursive=True,
             )
-            self.__observer.start()
+            self.OBSERVER.start()
 
-    def stop_observer(self):
-        if self.__observer is not None and self.__observer.is_alive():
-            self.__observer.stop()
-            self.__observer.join()
+    @classmethod
+    def stop_observer(cls):
+        if cls.OBSERVER is not None and cls.OBSERVER.is_alive():
+            cls.OBSERVER.stop()
+            cls.OBSERVER.join()
 
 
 class ServerFileHandler(FileSystemEventHandler):
 
-    def __init__(self, server_instance, logger_instance):
-        self.__server_instance = server_instance
-        self.__logger_instance = logger_instance
+    def __init__(self, reload_queue: queue.Queue):
+        self.__reload_queue = reload_queue
 
-    def on_modified(self, event):
-
+    def on_any_event(self, event):
         if event.is_directory:
             return
-        self.__logger_instance.info(f'File system has been modified.')
-        self.__server_instance.reload_server()
+
+        self.__reload_queue.put(True)
+
+
+class Adapter:
+    def __init__(self, reload_queue: queue.Queue):
+        self.__q = reload_queue
+
+    def fetch_signal(self):
+        while self.__q.empty():
+            sg = self.__q.get()
+            if sg:
+                return True
 
 
 if __name__ == '__main__':
-    server_instance = SockServer('127.0.0.1', 9999)
-    server_instance.start_serving()
-    print()
-    print()
-    print()
-    print()
+    q = queue.Queue()
 
+    obs = FSObserver(q)
+    obs.start_observer()
+
+    if q.empty():
+        server = SockServer('127.0.0.1', 9999)
+        server.start_serving()
+    else:
+        print('reloading')
+        print('reloading')
